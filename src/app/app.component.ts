b04bd67548dd927458969576cb3080c136149111
muncanjudit <muncanjudit@gmail.com>
2025-09-23 21:32:48 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
